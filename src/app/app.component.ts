4bfa45bd2dfa4e0a497d195078ac8d7b7bc836f2
Sarthak Bhatia <sarthakbhatia2018@gmail.com>
2021-04-25 20:07:07 +0530

    AL-1 Added Spinner (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCrud';
+  showLoader = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((routerEvent: Event) => {
+      switch (true) {
+        case routerEvent instanceof NavigationStart: {
+          this.showLoader = true;
+          break;
+        }
+        case routerEvent instanceof NavigationEnd:
+        case routerEvent instanceof NavigationCancel:
+        case routerEvent instanceof NavigationError: {
+          this.showLoader = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
 }
